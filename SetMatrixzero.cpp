970c4f7619a33bb5ba986d54c2048831c54b232c
Snehal7911 <gavadesnehal2@gmail.com>
2023-06-07 09:55:55 +0530

    Initial commit

---
+++ SetMatrixzero.cpp
@@ -0,0 +1,166 @@
+// Bruteforce solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public: // bruteforce
+    void markRow(vector<vector<int>> &matrix, int n, int m, int i)
+    {
+        // set all non-zero elements as -1 in the row i:
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    void markCol(vector<vector<int>> &matrix, int n, int m, int j)
+    {
+        // set all non-zero elements as -1 in the col j:
+        for (int i = 0; i < n; i++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    markRow(matrix, n, m, i);
+                    markCol(matrix, n, m, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == -1)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+//-------------------------------------**********************************-----------------------------------------------//
+
+// Better approach
+
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        // int row[n] = {0}; // row array
+        // int col[m] = {0}; // col array
+
+        vector<int> row(n, 0); // row array
+        vector<int> col(m, 0); // col array
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (row[i] || col[j])
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+//-------------------------------------**********************************-----------------------------------------------//
+
+// Oprtimal solution
+
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int col0 = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < m; j++)
+            {
+                if (matrix[i][j] != 0)
+                {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
